96647ee7efc2e55f86218cdc8b54ce7593bea0a4
José Lumbo <joselumbo77@gmail.com>
2022-05-24 21:25:44 -0300

    BlogpessoalReact

---
+++ src/paginas/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <>
+        <h1>Home</h1>
+        <img src="src\img\fotojose.jpeg" alt="Imagem pessoa" />
+        </>
+    );
+}
+
+export default Home;
