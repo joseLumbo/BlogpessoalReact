5a60606980f8f494d3bf3869bf67bb002892e80b
José Lumbo <joselumbo77@gmail.com>
2022-05-25 13:15:16 -0300

    Ataulização

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+function Home(){
+    return(
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://pbs.twimg.com/media/En-yIFBVoAMBdjw.jpg" alt="Imagem pessoa" />
+        </>
+    );
+}
+
+export default Home;
