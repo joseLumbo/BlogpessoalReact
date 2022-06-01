fa52a2a55a679e4e8bc64e390721d83e34458cff
José Lumbo <joselumbo77@gmail.com>
2022-06-01 11:34:52 -0300

    Blog atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-
         </Routes>
       </div>
       <Footer />
